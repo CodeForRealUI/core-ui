2358d92a3625835228886d562f9baae6504c3431
Liam Hatcher <liamhatcher@Liams-MBP.frontierlocal.net>
2018-03-04 18:31:22 -0600

    setup store

---
+++ app/reducers/index.js
@@ -0,0 +1,30 @@
+import { combineReducers } from 'redux';
+import { LOCATION_CHANGE } from 'react-router-redux';
+
+function a(state = 'lol', action) {
+    return state;
+}
+ 
+function b(state = 'wat', action) {
+    return state;
+}
+
+function routeReducer(state = 'null', action) {
+    switch (action.type) {
+        /* istanbul ignore next */
+        case LOCATION_CHANGE:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default function createReducer(injectedReducers) {
+    // Combine reducers inside here. 
+    return combineReducers({
+        a,
+        b,
+        route: routeReducer,
+        ...injectedReducers,
+    });
+}
