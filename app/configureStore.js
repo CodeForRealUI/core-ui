b30c24c805338723a91ae13969e17ce23307f3a7
DESKTOP-R9K5VO5\admin <jeremy_croff@live.nl>
2018-03-11 14:25:28 -0500

    folder structure update

--- app/configureStore.js
+++ app/configureStore.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
-import createReducer from './reducers';
+import createReducer from './data/reducers';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -51,7 +51,7 @@ export default function configureStore(initialState = {}, history) {
   // Make reducers hot reloadable, see http://mxs.is/googmo
   /* istanbul ignore next */
   if (module.hot) {
-    module.hot.accept('./reducers', () => {
+    module.hot.accept('./data/reducers', () => {
       store.replaceReducer(createReducer(store.injectedReducers));
     });
   }
