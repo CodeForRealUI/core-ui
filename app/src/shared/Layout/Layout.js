e61b35155568b5a47e00e1618e36b4b01d26b27c
DESKTOP-R9K5VO5\admin <jeremy_croff@live.nl>
2018-03-11 17:54:33 -0500

    bug fix for switching routes

--- app/src/shared/Layout/Layout.js
+++ app/src/shared/Layout/Layout.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Switch } from 'react-router-dom';
 
 import routes, { RecursiveRoutes } from '../../../routes';
 import NavBar from '../NavBar';
+import './styles.scss';
 class Layout extends Component {
 
-  componentWillReceiveProps(nextProps) {
-    const { location: { pathname: currentPathname } } = this.props;
-    const { location: { pathname: nextPathname } } = nextProps;
-  }
-
   render() {
-    const viewport =
-        routes.map((route) => (
-          <RecursiveRoutes
-            key={route.path}
-            {...route}
-          />
-        ));
+    const viewport = (
+      <div className="viewport-wrapper">
+        <Switch>
+          { routes.map((route) => (
+            <RecursiveRoutes
+              key={route.path}
+              {...route}
+            />
+        ))}
+        </Switch>
+      </div>
+    );
 
 
     return (
---
+++ app/src/shared/SignIn/SignIn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+function SignIn() {
+  return (
+    <div className="sign-in" >
+    sign in
+    </div>
+  );
+}
+
+SignIn.propTypes = {
+};
+
+export default (SignIn);
