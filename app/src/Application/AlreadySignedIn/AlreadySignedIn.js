e1f2789e24a378c827b83c19f18a9db1289ed757
DESKTOP-R9K5VO5\admin <jeremy_croff@live.nl>
2018-03-25 18:29:05 -0500

    lint fix

--- app/src/Application/AlreadySignedIn/AlreadySignedIn.js
+++ app/src/Application/AlreadySignedIn/AlreadySignedIn.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Paper } from 'material-ui';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { signoutRequest } from '~/data/actions/signout';
 import './styles.scss';
