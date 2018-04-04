fac118b3b1c69d8a1a0a4bad6f8609f6ce72c338
DESKTOP-R9K5VO5\admin <jeremy_croff@live.nl>
2018-04-03 23:41:30 -0500

    small classname changes

--- app/src/Application/NonProfitRoleSignup/NonProfitRoleSignup.js
+++ app/src/Application/NonProfitRoleSignup/NonProfitRoleSignup.js
@@ -26,7 +26,7 @@ class NonProfitRoleSignup extends Component {
     };
   }
 
-  handleSignUp = (e) => {
+  handleSubmit= (e) => {
     e.preventDefault();
     const { organizationName, websiteAddress, contactNumber, address1, address2, city, state, zipcode, organizationEmail, category } = this.state;
     // todo removing the +1 hack whenever we go international - this is to pass the backend validation.
@@ -57,127 +57,123 @@ class NonProfitRoleSignup extends Component {
   }
 
   renderForm = () => (
-
-    <div className="sign-up-fields">
-
-      <form onSubmit={this.handleSignUp}>
-        <FormGroup>
-          <h4 className="subheading">Organization Information</h4>
-          <Grid container spacing={24}>
-            <Grid item xs={6}>
-              <TextField
-                autoFocus
-                onChange={this.handleFieldChanged('organizationName')}
-                value={this.state.organizationName}
-                label="Organization name"
-                required
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={6} >
-              <TextField
-                label="Website address"
-                required
-                onChange={this.handleFieldChanged('websiteAddress')}
-                value={this.state.websiteAddress}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={6} >
-              <TextField
-                onChange={this.handleFieldChanged('contactNumber')}
-                value={this.state.contactNumber}
-                label="Contact Number"
-                type="tel"
-                required
-              />
-            </Grid>
-            <Grid item xs={6} >
-              <FormControl >
-                {/* todo make a required field for Submit */}
-                <InputLabel required htmlFor="category">Category</InputLabel>
-                <Select
-                  value={this.state.category}
-                  onChange={this.handleFieldChanged('category')}
-                  inputProps={{
-                    name: 'category',
-                  }}
-                >
-                  <MenuItem value={'Animal'}>Animal</MenuItem>
-                  <MenuItem value={'Environmental'}>Environmental</MenuItem>
-                  <MenuItem value={'Health'}>Health</MenuItem>
-                  <MenuItem value={'Education'}>Education</MenuItem>
-                  <MenuItem value={'Arts and Cultur'}>Arts and Culture</MenuItem>
-                  <MenuItem value={'Other'}>Other</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} >
-              <TextField
-                onChange={this.handleFieldChanged('organizationEmail')}
-                value={this.state.organizationEmail}
-                label="Organization Email"
-                type="email"
-                required
-              />
-            </Grid>
+    <form onSubmit={this.handleSubmit}>
+      <FormGroup>
+        <h4 className="subheading">Organization Information</h4>
+        <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <TextField
+              autoFocus
+              onChange={this.handleFieldChanged('organizationName')}
+              value={this.state.organizationName}
+              label="Organization name"
+              required
+              fullWidth
+            />
           </Grid>
-          <h4 className="subheading">Address Information</h4>
-          <Grid container spacing={24}>
-            <Grid item xs={6}>
-              <TextField
-                onChange={this.handleFieldChanged('address1')}
-                value={this.state.addres1}
-                label="Address 1"
-                required
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField
-                label="Address 2"
-                required
-                onChange={this.handleFieldChanged('address2')}
-                value={this.state.address2}
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                onChange={this.handleFieldChanged('city')}
-                value={this.state.city}
-                label="City"
-                required
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                label="State"
-                required
-                onChange={this.handleFieldChanged('state')}
-                value={this.state.state}
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                label="zipcode"
-                required
-                onChange={this.handleFieldChanged('zipcode')}
-                value={this.state.zipcode}
-              />
-            </Grid>
+          <Grid item xs={6} >
+            <TextField
+              label="Website address"
+              required
+              onChange={this.handleFieldChanged('websiteAddress')}
+              value={this.state.websiteAddress}
+              fullWidth
+            />
           </Grid>
-          <button
+          <Grid item xs={6} >
+            <TextField
+              onChange={this.handleFieldChanged('contactNumber')}
+              value={this.state.contactNumber}
+              label="Contact Number"
+              type="tel"
+              required
+            />
+          </Grid>
+          <Grid item xs={6} >
+            <FormControl >
+              {/* todo make a required field for Submit */}
+              <InputLabel required htmlFor="category">Category</InputLabel>
+              <Select
+                value={this.state.category}
+                onChange={this.handleFieldChanged('category')}
+                inputProps={{
+                  name: 'category',
+                }}
+              >
+                <MenuItem value={'Animal'}>Animal</MenuItem>
+                <MenuItem value={'Environmental'}>Environmental</MenuItem>
+                <MenuItem value={'Health'}>Health</MenuItem>
+                <MenuItem value={'Education'}>Education</MenuItem>
+                <MenuItem value={'Arts and Cultur'}>Arts and Culture</MenuItem>
+                <MenuItem value={'Other'}>Other</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} >
+            <TextField
+              onChange={this.handleFieldChanged('organizationEmail')}
+              value={this.state.organizationEmail}
+              label="Organization Email"
+              type="email"
+              required
+            />
+          </Grid>
+        </Grid>
+        <h4 className="subheading">Address Information</h4>
+        <Grid container spacing={24}>
+          <Grid item xs={6}>
+            <TextField
+              onChange={this.handleFieldChanged('address1')}
+              value={this.state.addres1}
+              label="Address 1"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Address 2"
+              required
+              onChange={this.handleFieldChanged('address2')}
+              value={this.state.address2}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              onChange={this.handleFieldChanged('city')}
+              value={this.state.city}
+              label="City"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              label="State"
+              required
+              onChange={this.handleFieldChanged('state')}
+              value={this.state.state}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              label="zipcode"
+              required
+              onChange={this.handleFieldChanged('zipcode')}
+              value={this.state.zipcode}
+            />
+          </Grid>
+        </Grid>
+        <button
                // todo check for the entire form and dont rely on html validation
-            disabled={!this.state.category}
-            className="submit-button"
-          >
+          disabled={!this.state.category}
+          className="submit-button"
+        >
             Submit
           </button>
-        </FormGroup>
-      </form>
-    </div>
+      </FormGroup>
+    </form>
     )
 
   render() {
