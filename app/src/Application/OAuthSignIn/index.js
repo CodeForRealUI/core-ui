cf7f09fdd942fedca2ac2b42d9f97b4ef2ec369c
DESKTOP-R9K5VO5\admin <jeremy_croff@live.nl>
2018-04-12 19:49:33 -0500

    small lint fix

--- app/src/Application/OAuthSignIn/index.js
+++ app/src/Application/OAuthSignIn/index.js
@@ -1,7 +1,8 @@
 import Loadable from 'react-loadable';
 import Spinner from 'src/shared/Spinner';
+import OauthSignIn from './OAuthSignIn';
 
 export default Loadable({
-  loader: () => import('./OauthSignIn'),
+  loader: () => import(OauthSignIn),
   loading: Spinner,
 });
