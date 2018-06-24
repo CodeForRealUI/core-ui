272284c6fe658f68701b15d2f98426b47db213f6
Liam Hatcher <liamhatcher@Liams-MacBook-Pro.local>
2018-06-24 12:17:03 -0500

    test commit

--- app/src/Application/Dashboard/ProjectViewer/ProjectViewer.js
+++ app/src/Application/Dashboard/ProjectViewer/ProjectViewer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.scss';
 
 const ProjectViewer = () => (
-  <div className="project-viewer">test</div>
+  <div className="project-viewer">testing</div>
 );
 
 export default ProjectViewer;
